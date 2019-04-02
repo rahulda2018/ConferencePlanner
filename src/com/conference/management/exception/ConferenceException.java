4482699217f879803ac91e1afed6380b47b01e7c
Rahul Das Adhikary <rahul.das.adhikary@gmail.com>
2019-04-02 21:18:17 +0530

    Add files via upload

--- src/com/conference/management/exception/ConferenceException.java
+++ src/com/conference/management/exception/ConferenceException.java
@@ -1,1 +1,31 @@
+package com.conference.management.exception;
 
+/**
+ * Custom Exception for using within the application
+ *
+ */
+public class ConferenceException extends Exception {
+
+	public ConferenceException() {
+		super();
+	}
+
+	public ConferenceException(String message) {
+		super(message);
+	}
+
+	public ConferenceException(Throwable cause) {
+		super(cause);
+	}
+
+	public ConferenceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ConferenceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+	
+	
+
+}
